d0589d43277f4c6912031488a11ec303a2fb3469
kaushik912 <kaushik.1986@gmail.com>
2024-12-05 14:44:38 +0530

    Create image_processing_sample.py

---
+++ image_processing_sample.py
@@ -0,0 +1,51 @@
+{
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "source": [
+        "Here is one approach to do GenAI Practice.\n",
+        "First you create a sample file like this in github.\n",
+        "And then run your GENAI based commands.\n",
+        "This should run ChatGPT models."
+      ],
+      "metadata": {
+        "id": "cR-LhRljMd7n"
+      },
+      "id": "cR-LhRljMd7n"
+    },
+    {
+      "cell_type": "markdown",
+      "id": "8d5f3703",
+      "metadata": {
+        "id": "8d5f3703"
+      },
+      "source": [
+        "[![Open in Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/langchain-ai/langchain-academy/blob/main/module-1/simple-graph.ipynb) [![Open in LangChain Academy](https://cdn.prod.website-files.com/65b8cd72835ceeacd4449a53/66e9eba12c7b7688aa3dbb5e_LCA-badge-green.svg)](https://academy.langchain.com/courses/take/intro-to-langgraph/lessons/58238187-lesson-2-simple-graph)"
+      ]
+    }
+  ],
+  "metadata": {
+    "kernelspec": {
+      "display_name": "Python 3 (ipykernel)",
+      "language": "python",
+      "name": "python3"
+    },
+    "language_info": {
+      "codemirror_mode": {
+        "name": "ipython",
+        "version": 3
+      },
+      "file_extension": ".py",
+      "mimetype": "text/x-python",
+      "name": "python",
+      "nbconvert_exporter": "python",
+      "pygments_lexer": "ipython3",
+      "version": "3.12.1"
+    },
+    "colab": {
+      "provenance": []
+    }
+  },
+  "nbformat": 4,
+  "nbformat_minor": 5
+}
